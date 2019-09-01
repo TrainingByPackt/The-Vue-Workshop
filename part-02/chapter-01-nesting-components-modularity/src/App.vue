f2a9fbb68a3928ae1d675a53fc5044f278d31769
Sang Dang <sangdth@gmail.com>
2019-09-01 21:16:26 +0300

    move folder

---
+++ part-02/chapter-01-nesting-components-modularity/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <Message content="This text is not hardcoded" />
+  </div>
+</template>
+
+<script>
+import Message from './components/Message.vue';
+
+export default {
+  name: 'app',
+  components: {
+    Message,
+  },
+};
+</script>
---
+++ part-02/chapter-01-nesting-components-modularity/src/components/Message.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="message-wrapper">
+    <p class="message-content">
+      {{ content }}
+      <small>12.8.2019</small>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Message',
+  props: {
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
